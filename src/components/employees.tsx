7d35992e6a3e79bfe5489bbc94f67491975e06e8
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-12 01:26:34 +0100

    Add shadcn Components

---
+++ src/components/employees.tsx
@@ -0,0 +1,45 @@
+import { api } from "~/utils/api";
+import { Skeleton } from "./ui/skeleton";
+
+export default function EmployeeList() {
+  const { data: employees, isLoading } = api.employees.getAll.useQuery();
+  if (isLoading) {
+    return (
+      <div className="space-y-4">
+        <div className="mt-2 w-fit space-y-2 rounded-md border-2 border-gray-50 p-4">
+          <div className="flex gap-2">
+            <Skeleton className="h-8 w-96" />
+            <Skeleton className="h-8 w-32" />
+          </div>
+          <Skeleton className="h-8 w-72" />
+        </div>
+
+        <div className="mt-2 w-fit space-y-2 rounded-md border-2 border-gray-50 p-4">
+          <div className="flex gap-2">
+            <Skeleton className="h-8 w-96" />
+            <Skeleton className="h-8 w-32" />
+          </div>
+          <Skeleton className="h-8 w-72" />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {employees?.map((employee) => (
+        <div
+          className=" mt-2 w-fit rounded-md border-2 p-2  "
+          key={employee.id}
+        >
+          <div className="mb-2 flex items-center gap-2">
+            <h1 className="text-2xl font-bold">{employee.name}</h1>{" "}
+            <div className="rounded-lg bg-emerald-200 p-1 px-4 text-base font-bold text-emerald-800">
+              {employee.type}
+            </div>
+          </div>
+          <h2>{employee.email}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/form.tsx
@@ -0,0 +1,134 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { api } from "~/utils/api";
+
+export const FormSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .max(20, {
+      message: "A Name can have 20 Characters at most",
+    }),
+  email: z.string().email({ message: "Please type a valid email" }),
+  type: z.string({ required_error: "Please Select a User Type" }),
+});
+
+export function NewEmployeeForm() {
+  const { mutateAsync } = api.employees.form.useMutation();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      type: "dev",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    await mutateAsync(data);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+    form.reset({
+      name: "",
+      email: "",
+      type: "dev",
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-fit space-y-2 rounded-md  bg-slate-50 p-4 "
+      >
+        <div className="flex gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{field.name}</FormLabel>
+                <FormControl>
+                  <Input placeholder="Rick Astley" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Please insert the employee&apos; full name
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{field.name}</FormLabel>
+                <FormControl>
+                  <Input placeholder="rick@astley.com" {...field} />
+                </FormControl>
+                <FormDescription>Type the user&apos; email</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Employee Position</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="spaceduck" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="intern">Intern</SelectItem>
+                  <SelectItem value="dev">Web Developer</SelectItem>
+                  <SelectItem value="designer">Web Designer</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Contact the manager if there are missing open positions
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
